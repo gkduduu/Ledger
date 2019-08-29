13e9ffb445adde6491646bea423495ba85811413
gkdud <gkduduu@nvaer.com>
2019-08-29 22:03:12 +0900

    스플레ㅅ, 리스트화면 생성

---
+++ app/src/main/java/utils/jhy/ledger/SplashActivity.java
@@ -0,0 +1,12 @@
+package utils.jhy.ledger;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
